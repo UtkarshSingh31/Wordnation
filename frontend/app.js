b487c88c221fe91831884d6ab316bf60aa245a57
Utkarsh Singh <utkarshsingh.0902@gmail.com>
2026-02-09 23:29:41 +0530

    Update API_BASE URL to use production endpoint

--- frontend/app.js
+++ frontend/app.js
@@ -1,7 +1,7 @@
 (function () {
   "use strict";
 
-  const API_BASE = "http://127.0.0.1:8000";
+  const API_BASE = "https://wordnation.onrender.com";
   const MEANING_URL = `${API_BASE}/v1/vocab/meaning`;
   const HEALTH_URL = `${API_BASE}/health`;
 
