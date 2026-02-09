5aad7f5ffe3913ebe1c1ccbff0db42e28012bc31
Utkarsh Singh <utkarshsingh.0902@gmail.com>
2026-02-09 23:03:34 +0530

    Remove exception handling for model loading in provider.py

--- app/services/llm/provider.py
+++ app/services/llm/provider.py
@@ -2,9 +2,6 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 
 load_dotenv()
+model=ChatGoogleGenerativeAI(model='gemini-2.5-flash')
 
-try:
-    model=ChatGoogleGenerativeAI(model='gemini-2.5-flash')
-except Exception :
-    print(f"Model not loaded")
     
